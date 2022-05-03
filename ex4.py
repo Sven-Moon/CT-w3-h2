7349f77363418cefd786c2051003259732ea330e
Svenpire <svenpire@gmail.com>
2022-05-03 14:55:18 -0600

    fix(fib)

--- ex4.py
+++ ex4.py
@@ -10,12 +10,14 @@
 
 def fib(n):
     if n > 1:
-        return n + fib(n-1)
+        return fib(n-1) + fib(n-2)
     else: 
         return 1
     
+print(fib(4))
 print(fib(5))
 print(fib(6))
 print(fib(10))
 print(fib(20))
 print(fib(21))
+print(fib(22))
